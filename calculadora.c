6843993b96d95f4be126283f759577d273bb9bc5
ihriel.mosko <80472864+ihrielmosko@users.noreply.github.com>
2022-04-01 14:11:07 -0300

    agora tem potência
    tem muuuita gambiarra na potência, mas funciona

--- calculadora.c
+++ calculadora.c
@@ -1,45 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main () {
-    int x;
-    int y;
-    int z;
+    int x, x2, y, z, i;
     float resultado;
+
     printf("digite um numero inteiro\n");
     scanf("%d", &x);
-    printf("escolha o operador digitando o respectivo numero\n1(soma) 2(subtracao) 3(multiplicacao) 4(divisao) 5(modulo)\n");
+    printf("escolha o operador digitando o respectivo numero\n0(soma) 1(subtracao) 2(multiplicacao) 3(divisao) 4(resto) 5(potencia)\n");
     scanf("%d", &z);
     printf("digite o segundo numero\n");
     scanf("%d", &y);
+        
     ///*
-    if (z == 1) {
+    if (z == 0) {
         resultado = x + y;
-        floor(resultado);
         printf("%f", resultado);
-    } else if (z == 2) {
+
+    } else if (z == 1) {
         resultado = x - y;
-        floor(resultado);
         printf("%f", resultado);
-    } else if (z == 3) {
+
+    } else if (z == 2) {
         resultado = x * y;
-        floor(resultado);
         printf("%f", resultado);
-    } else if (z == 4) {
+
+    } else if (z == 3) {
         if (x == 0, y == 0) {
             printf("nao e possivel dividir por 0");
+
         } else {
             resultado = x / y;
-            floor(resultado);
             printf("%f", resultado);
         }
-    } else if (z == 5) {
+
+    } else if (z == 4) {
         resultado = x % y;
-        floor(resultado);
         printf("%f", resultado);
+
+    } else if (z == 5) {
+        x2 = x;
+
+        while (y != 1) {
+            if (y < 0){
+                printf("expoente negativo indisponivel");
+                break;
+            } else if (y == 0) {
+                printf("1");
+                break;
+            } else {
+                x2 = x2 * x;
+                y--;
+                resultado = x2;
+            }
+            if (y = 1) {
+                if (x < 0) {
+                    resultado = resultado/-1;
+                    printf("%f\n", resultado);
+                } else {
+                    printf("%f\n", resultado);
+                }
+            } else {}
+        }
     } else {
         printf("este nao e um operador valido");
     }
     //*/
+    return 0;
 }
